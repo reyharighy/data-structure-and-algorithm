df898bc219290e648bbe5870d61be61c66b0eb96
almahbubyanwar <almahbubyanwar@protonmail.com>
2024-04-26 09:04:56 +0700

    temporary main file?

--- main.cpp
+++ main.cpp
@@ -3,5 +3,8 @@
 #include <iostream>
 
 int main() {
-        
+    std::cout << "Aplikasi Linked List" << "\n";
+    // std::cout << "what the fuck am i supposed to do from here" << "\n";
+    std::cout << "(1) String Reversal | (2) Stack Implementation | (3) Queue Implementation | "
+    << "(4) Ordered Data Input | (5) Doubly Linked List";
 }
