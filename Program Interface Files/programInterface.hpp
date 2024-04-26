6dfcd60727e4f5c0904c48e43a7f9deaf2adeb47
Reyhan Arighy <arighymoch@gmail.com>
2024-04-26 23:45:36 +0700

    Update programInterface.hpp
    - minor changes on comments
    - add subProgramSelection method
    
    Signed-off-by: Reyhan Arighy <arighymoch@gmail.com>

--- Program Interface Files/programInterface.hpp
+++ Program Interface Files/programInterface.hpp
@@ -1,6 +1,8 @@
 /*
     File ini digunakan sebagai antarmuka untuk memudahkan proses penjelajahan ke berbagai
-    subprogram dari seluruh chapter yang ditugaskan pada mata pelajaran ini
+    subprogram dari seluruh chapter yang ditugaskan pada mata pelajaran ini. Berperan juga sebagai
+    base class untuk seluruh derived class yang menjalankan tiap subprogram. Dengan kata lain, juga
+    berperan dalam menyediakan fungsi abstraksi dalam menjalankan tiap fungsi yang tersedia.
 */
 
 #ifndef PROGRAM_INTERFACE_H
@@ -9,15 +11,17 @@
 #include <iostream>
 #include <string>
 #include <map>
+#include <memory>
 
 /*
     Deklarasi awal dari objek Program dalam menjalankan setiap subprogram pada tiap chapter.
 */
 
-class Program {
+class Program { 
 public:
-    virtual void start() = 0; // Metode abstraksi untuk memulai tiap chapter atau subprogram
+    virtual void start() = 0; // Metode polymorphism untuk tiap derived class
     short taskList(const short*); // Memberikan daftar subprogram di dalam suatu chapter
+    bool subProgramSelection(const short*, std::string*, std::map<const short, Program*>*); // Menjalankan subprogram yang dipilih
 };
 
 #endif
