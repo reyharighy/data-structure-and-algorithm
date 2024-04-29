90b87d6d06c8af8c708aa36d03874541ca163b35
Reyhan Arighy <arighymoch@gmail.com>
2024-04-30 00:24:45 +0700

    Update customUtility.hpp
    minor changes on comments
    
    Signed-off-by: Reyhan Arighy <arighymoch@gmail.com>

--- Custom Utility Files/customUtility.hpp
+++ Custom Utility Files/customUtility.hpp
@@ -1,7 +1,6 @@
 /*
-    File ini digunakan sebagai antarmuka untuk menyimpan berbagai fungsi dasar
-    yang berkaitan dengan standard input dari pengguna. Selain itu, file ini juga
-    berperan dalam memenuhi proses abstraksi dari tiap fungsi yang disediakan.
+    File ini digunakan sebagai antarmuka untuk menyimpan berbagai fungsi dasar yang berkaitan dengan standard input dari pengguna. 
+    Selain itu, file ini juga berperan dalam memenuhi proses abstraksi dari tiap fungsi yang disediakan.
 */
 
 #ifndef CUSTOM_UTILITY_H
@@ -12,16 +11,19 @@
 #include <sstream>
 #include <charconv>
 
-/*
-    Deklarasi awal untuk berbagai fungsi dari file "customUtility.cpp".
-    Definisi tiap fungsi ada di file "customUtility.cpp".
-*/
+/*----------------------------------------------------------------------------------------------------------------------------------------
+    PART 1: Deklarasi awal untuk berbagai fungsi dari file "customUtility.cpp". Definisi tiap fungsi ada di file "customUtility.cpp".
+----------------------------------------------------------------------------------------------------------------------------------------*/
 
-std::string normalizeInput();
-long inputIntValidator(std::string*);
-void outputBuffer();
-void invalidMenuChosen(short*, std::string*);
-std::string titleCase();
-void setCapacityValue(bool*, int*, int*, std::string*, std::string**);
+std::string normalizeInput(); // Definisi ada di PART 1
+long inputIntValidator(std::string*); // Definisi ada di PART 2
+void outputBuffer(); // Definisi ada di PART 3
+void invalidMenuChosen(short*, std::string*); // Mengacu ke PART 4
+std::string titleCase(); // Definisi ada di PART 5
+void setCapacityValue(bool*, int*, int*, std::string*, std::string**); // Definisi ada di PART 6
+
+/*----------------------------------------------------------------------------------------------------------------------------------------
+    END OF SCOPE FOR PART 1.
+----------------------------------------------------------------------------------------------------------------------------------------*/
 
 #endif 
