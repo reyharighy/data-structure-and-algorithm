929b59d793f4e10cca4ce6e5ae333f2804617bb2
almahbubyanwar <almahbubyanwar@protonmail.com>
2024-05-09 13:04:27 +0700

    linked list problem 3 completed

--- Linked List Chapter Files/linkedListFiveThree.cpp
+++ Linked List Chapter Files/linkedListFiveThree.cpp
@@ -8,10 +8,14 @@ namespace LinkedListChapter {
     namespace QueueDemo {
         void display(Implementations::Queue<std::string> q) {
             auto output = q;
+            if (output.isEmpty()) {
+                std::cout << "(kosong)";
+            }
             while (!output.isEmpty()) {
                 std::cout << output.dequeue() << "; ";
             }
         }
+
         int demo() {
             std::cout << "Masukkan banyaknya anggota maks.: ";
             size_t size;
@@ -31,12 +35,21 @@ namespace LinkedListChapter {
 
                         std::cout << "Masukkan data: ";
                         std::getline(std::cin, input);
-                        q.enqueue(input);
+                        try {
+                            q.enqueue(input);
+                        } catch (const std::out_of_range& e) {
+                            std::cout << "Antrian penuh.\n";
+                        }
                         break;
                     }
                     case 2: {
-                        std::string data = q.dequeue();
-                        std::cout << "Data yang diambil: " << data;
+                        try {
+                            std::string data = q.dequeue();
+                            std::cout << "Data yang diambil: " << data << "\n";
+                        }
+                        catch (const std::out_of_range& e) {
+                            std::cout << "Antrian kosong.\n";
+                        }
                         break;
                     }
                     case 3:
