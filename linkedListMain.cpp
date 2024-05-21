67ff8ef1ade6c7fbd2b406ba3a47f71b473f018f
almahbubyanwar <almahbubyanwar@protonmail.com>
2024-05-21 20:38:01 +0700

    revert from merge

--- linkedListMain.cpp
+++ linkedListMain.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     while (true) {
-        std::cout << "Aplikasi Linked List" << "\n";
+        std::cout << "Aplikasi Implementasi Linked List" << "\n";
         // std::cout << "what the fuck am i supposed to do from here" << "\n";
         std::cout << "(1) String Reversal | (2) Stack Implementation | (3) Queue Implementation | "
         << "(4) Ordered Data Input | (5) Doubly Linked List | (6) Infix-Postfix Processing | (7) Kode-Nama | (8) Keluar\n";
